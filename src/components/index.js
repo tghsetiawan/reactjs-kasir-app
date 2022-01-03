04047e136541b900026b1a6eccd773cd0abe08f8
tghsetiawan <55044847+tghsetiawan@users.noreply.github.com>
2022-01-03 12:15:19 +0700

    video 4

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ import Hasil from './Hasil'
 import ListCategories from './ListCategories'
 import NavbarComponent from './NavbarComponent'
 
-export {Hasil, ListCategories, NavbarComponent}
+export { Hasil, ListCategories, NavbarComponent }
