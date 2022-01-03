a9d91302f588a95a49e037457538027e123362e4
tghsetiawan <55044847+tghsetiawan@users.noreply.github.com>
2022-01-03 15:03:50 +0700

    video 5 - menampilkan menu produk

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
+import React, { Component } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import "./App.css";
-import {Hasil, ListCategories, NavbarComponent} from "./components";
+import { Hasil, ListCategories, Menus, NavbarComponent } from "./components";
+import { API_URL } from "./utils/constants";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-              </h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "products")
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { menus } = this.state;
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4>
+                  <strong>Daftar Produk</strong>
+                </h4>
+                <hr />
+                <Row>
+                  {menus && menus.map((menu) => (
+                  // <h2>{menu.nama}</h2>
+                  <Menus key={menu.id} menu={menu} />
+                  ))}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
